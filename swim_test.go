7e4341dde9c9f15c21730e8e136f0707f35518d6
eyazici90 <e.yazici1990@gmail.com>
2024-08-05 11:22:23 +0200

    test: assert via metrics on swim tests

--- swim_test.go
+++ swim_test.go
@@ -1,15 +1,26 @@
 package swim_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/eyazici90/swim"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSwim_New(t *testing.T) {
-	ms, err := swim.New()
+	ctx := context.Background()
 
+	ms1, err := swim.New(nil)
 	assert.NoError(t, err)
-	assert.NotNil(t, ms)
+
+	ms2, err := swim.New(nil)
+	err = ms2.Join(ctx, ms1.Me().Addr().String())
+	assert.NoError(t, err)
+
+	<-time.After(time.Millisecond * 10)
+
+	assert.Greater(t, ms1.Metrics().ReceivedNum, uint32(0))
+	assert.Greater(t, ms2.Metrics().SentNum, uint32(0))
 }
