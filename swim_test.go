44ac108a70e6ec10dd0528494d435a30d4785bd6
eyazici90 <e.yazici1990@gmail.com>
2024-08-06 16:17:05 +0200

    test: refactor membership joining test case

--- swim_test.go
+++ swim_test.go
@@ -7,19 +7,23 @@ import (
 
 	"github.com/eyazici90/swim"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestSwim_New(t *testing.T) {
+func TestSwim_Join(t *testing.T) {
 	ctx := context.Background()
 
 	ms1, err := swim.New(nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
+	defer ms1.Stop()
 
 	ms2, err := swim.New(nil)
+	require.NoError(t, err)
+	defer ms2.Stop()
 	err = ms2.Join(ctx, ms1.Me().Addr().String())
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
-	<-time.After(time.Millisecond * 10)
+	<-time.After(time.Millisecond * 150)
 
 	assert.Greater(t, ms1.Metrics().ReceivedNum, uint32(0))
 	assert.Greater(t, ms2.Metrics().SentNum, uint32(0))
