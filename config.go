ec1d6fecae3c883b4763f04b15b40fb66da96392
eyazici90 <e.yazici1990@gmail.com>
2024-08-13 10:25:34 +0200

    chore: introduce defaultconfig func
    To be used by clients to ease usage of the lib.

--- config.go
+++ config.go
@@ -5,26 +5,30 @@ import (
 	"time"
 )
 
+func DefaultConfig() *Config {
+	return &Config{
+		MaxSuspicionCount: 5,
+		GossipRatio:       20,
+		GossipInterval:    time.Millisecond * 20,
+		IOTimeout:         time.Millisecond * 100,
+		OnJoin: func(_ net.Addr) {
+		},
+		OnLeave: func(_ net.Addr) {
+		},
+	}
+}
+
 type Config struct {
 	Port              uint16 // binding lister to
 	MaxSuspicionCount int
 	GossipInterval    time.Duration // duration of gossiping with members
 	GossipRatio       uint8         // min. percentage of gossiping active members concurrently. ex: 20 => %20
 	IOTimeout         time.Duration
-	OnJoin, OnLeave   func(m net.Addr)
+	OnJoin, OnLeave   func(addr net.Addr)
 }
 
 func setDefaults(ptr **Config) {
 	if *ptr == nil {
-		*ptr = &Config{
-			MaxSuspicionCount: 5,
-			GossipRatio:       20,
-			GossipInterval:    time.Millisecond * 20,
-			IOTimeout:         time.Millisecond * 100,
-			OnJoin: func(_ net.Addr) {
-			},
-			OnLeave: func(_ net.Addr) {
-			},
-		}
+		*ptr = DefaultConfig()
 	}
 }
