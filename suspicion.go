fcd7a3d06a9b6946d6ef5848e9b9d87c21af6722
eyazici90 <e.yazici1990@gmail.com>
2025-01-04 20:30:53 +0100

    fix: return early if lives len is 0
    This means, there is no alive peer to request for indirect ping.

--- suspicion.go
+++ suspicion.go
@@ -7,11 +7,14 @@ import (
 
 func (ms *Membership) suspect(ctx context.Context, member Member) (alive bool) {
 	ms.setState(statusSuspect, member.Addr())
-	req := indirectPingReq{sender: ms.me.Addr(), suspect: member.Addr()}
 	lives := ms.alives()
+	if len(lives) == 0 {
+		return false
+	}
 	num := rand.Int() % len(lives)
 	target := lives[num]
 	resp := make([]byte, 1)
+	req := indirectPingReq{sender: ms.me.Addr(), suspect: member.Addr()}
 	if err := sendReceiveTCP(ctx, target.Addr(), req.encode(), resp); err != nil {
 		return false
 	}
